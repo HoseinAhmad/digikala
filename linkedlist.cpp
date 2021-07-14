d7bd6fa57aa30e78070f31b9f45b0b760155aa4b
HoseinAhmad <Psdk**136757>
2021-07-14 17:06:42 +0430

    write add_buyer_node function

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -38,3 +38,36 @@ void add_seller_node(seller* head,string name, string username,string password,s
 		}
 	}
 }
+void add_buyer_node(buyer* head, string username, string password, string address, int phone) {
+	// insert it to the end of list
+	buyer* curr = new buyer();
+	curr = head;
+	if (head == NULL)
+	{
+		head->setusername(username);
+		head->setpassword(password);
+		head->setaddress(address);
+		head->setphone(phone);
+		head->setwallet(0);
+		head->next = NULL;
+	}
+	else
+	{
+		while (true)
+		{
+			if (curr->next == NULL)
+			{
+				curr->next = new buyer();
+				curr->next->setusername(username);
+				curr->next->setpassword(password);
+				curr->next->setaddress(address);
+				curr->next->setphone(phone);
+				curr->next->setwallet(0);
+				curr->next->next = NULL;
+
+				break;
+			}
+			curr = curr->next;
+		}
+	}
+}
--- linkedlist.h
+++ linkedlist.h
@@ -2,11 +2,15 @@
 #define linkedlist.h
 #include "linkedlist.h"
 #include"seller.h"
+#include"buyer.h"
 #include<string>
 
 using namespace std;
 
 void add_seller_node(seller* head,string name, string username, string password,string address,int phone);
+void add_buyer_node(buyer* head,string username, string password, string address, int phone);
+void add_product_node();
+void delet_product_node();
 
 #endif // !linkedlist.h
 
